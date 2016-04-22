32a8ad2643d379a34084c5cb45c59cdce9ba59e6
Lukjar <lukjar92@gmail.com>
2016-04-22 16:00:15 +0200

    Added nfc reader

--- smart-activities/app/src/main/java/hamsters/magic/smart_activities/MainActivity.java
+++ smart-activities/app/src/main/java/hamsters/magic/smart_activities/MainActivity.java
@@ -1,21 +1,34 @@
 package hamsters.magic.smart_activities;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import android.widget.Toast;
 import junit.framework.Test;
+import model.NfcConstants;
+import nfc.NdefReaderTask;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NfcAdapter mNfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +46,71 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        initNfcAdapter();
+        handleIntent(getIntent());
     }
 
     private void setUpActivitiesListContent() {
         ListView activityListView  = (ListView) findViewById(R.id.activities_list);
         KidActivityListAdapter kidActivityListAdapter = new KidActivityListAdapter(this.getApplicationContext());
         activityListView.setAdapter(kidActivityListAdapter);
+    }
+
+    private void initNfcAdapter() {
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (mNfcAdapter == null) {
+            // Stop here, we definitely need NFC
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setupForegroundDispatch(this, mNfcAdapter);
+    }
+
+    @Override
+    protected void onPause() {
+        stopForegroundDispatch(this, mNfcAdapter);
+        super.onPause();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        String action = intent.getAction();
+        Log.d(NfcConstants.TAG, action);
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            String type = intent.getType();
+            if (NfcConstants.MIME_TEXT_PLAIN.equals(type)) {
+                Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+                new NdefReaderTask().execute(tag);
+            } else {
+                Log.d(NfcConstants.TAG, "Wrong mime type: " + type);
+            }
+        } else if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action) || NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)) {
+            // In case we would still use the Tech Discovered Intent
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            new NdefReaderTask().execute(tag);
+        }
+    }
+
+    public static void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        final Intent intent = new Intent(activity, activity.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        final PendingIntent pendingIntent = PendingIntent.getActivity(activity, 0, intent, 0);
+
+        adapter.enableForegroundDispatch(activity, pendingIntent, null, null);
+    }
+
+    public static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        adapter.disableForegroundDispatch(activity);
     }
 
     @Override
