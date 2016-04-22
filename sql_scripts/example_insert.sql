0206087d6ff8a7a5650603571515686cf05ab9a0
Mikolaj Lewandowski <mikolevy1@gmail.com>
2016-04-22 20:54:38 +0200

    Fix db

--- sql_scripts/example_insert.sql
+++ sql_scripts/example_insert.sql
@@ -1,26 +1,3 @@
-CREATE TABLE manager_action (
-	id				BIGSERIAL PRIMARY KEY,
-	name			TEXT NOT NULL,
-	img_url			TEXT NOT NULL DEFAULT 'picture',
-	order_number	INTEGER NOT NULL,
-	kid_activity_id BIGINT REFERENCES manager_kid_activity(id) ON DELETE CASCADE
-);
-
-CREATE TABLE manager_nfc_device (
-	id				BIGSERIAL PRIMARY KEY,
-	device_id		INTEGER NOT NULL, DEFAULT 555,
-    kid_activity_id BIGINT REFERENCES manager_kid_activity(id) ON DELETE CASCADE
-);
-
-CREATE TABLE manager_kid_activity (
-	id				BIGSERIAL PRIMARY KEY,
-    name			TEXT NOT NULL,
-    img_url			TEXT NOT NULL DEFAULT 'picture',
-    order_number	INTEGER NOT NULL,
-    is_done			BOOLEAN NOT NULL DEFAULT FALSE
-);
-
-
 INSERT INTO manager_kid_activity (name, img_url, order_number) VALUES
 	('Mycie zębów', 'szczoteczka', 1),
 	('Zabawa z misiem', 'mis', 2),
